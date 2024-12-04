64d186128faf9c0caf5cc83960b36ffa2d5a6fb0
cxnt <pavelbartsits@gmail.com>
2024-12-04 17:34:27 +0400

    Update inference settings file format and add model export options for ONNX and TensorRT

--- supervisely_integration/train/main.py
+++ supervisely_integration/train/main.py
@@ -28,12 +28,16 @@ train.register_inference_class(RTDETRv2Benchmark, inference_settings)
 
 # For debug
 # app_state = {
-#     "input": {"project_id": 42201},
+#     "input": {"project_id": 43192},
 #     "train_val_split": {"method": "random", "split": "train", "percent": 80},
-#     "classes": ["dog", "horse", "cat", "squirrel", "sheep"],
+#     "classes": ["apple"],
 #     "model": {"source": "Pretrained models", "model_name": "RT-DETRv2-S"},
 #     "hyperparameters": "epoches: 2\nbatch_size: 16\neval_spatial_size: [640, 640]  # height, width\n\ncheckpoint_freq: 5\nsave_optimizer: false\nsave_ema: false\n\noptimizer:\n  type: AdamW\n  lr: 0.0001\n  betas: [0.9, 0.999]\n  weight_decay: 0.0001\n\nclip_max_norm: 0.1\n\nlr_scheduler:\n  type: MultiStepLR  # CosineAnnealingLR | OneCycleLR\n  milestones: [35, 45]  # epochs\n  gamma: 0.1\n\nlr_warmup_scheduler:\n  type: LinearWarmup\n  warmup_duration: 1000  # steps\n\nuse_ema: True \nema:\n  type: ModelEMA\n  decay: 0.9999\n  warmups: 2000\n\nuse_amp: True\n",
-#     "options": {"model_benchmark": {"enable": True, "speed_test": True}, "cache_project": True},
+#     "options": {
+#         "model_benchmark": {"enable": True, "speed_test": True},
+#         "cache_project": True,
+#         "export": {"onnx": True, "tensorrt": True},
+#     },
 # }
 # train.gui.load_from_app_state(app_state)
 
@@ -67,6 +71,22 @@ def start_training():
         "best_checkpoint": "best.pth",
     }
     return experiment_info
+
+
+@train.export_onnx
+def export_onnx():
+    print("---------------------------------------------")
+    print("Exporting ONNX for RT-DETRv2")
+    print("---------------------------------------------")
+    return "path/to/onnx"
+
+
+@train.export_tensorrt
+def export_tensorrt():
+    print("---------------------------------------------")
+    print("Exporting TensorRT for RT-DETRv2")
+    print("---------------------------------------------")
+    return "path/to/tensorrt"
 
 
 def convert_data():
